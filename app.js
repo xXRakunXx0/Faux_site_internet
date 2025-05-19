2a731572efcc464c0c5fd82d3cb2e067ae2e5c7b
xXRakunXx0 <alan.dedin42@gmail.com>
2025-05-19 17:22:30 +0200

    fini_il_manque_des_detail

---
+++ app.js
@@ -0,0 +1,29 @@
+// Récupérer les produits depuis l'API Fake Store
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  
+  .then(data =>{
+    data.forEach(element => {
+        MaCarte(element)
+    });
+    console.log(data[0].category);
+    
+  });
+
+// Fonction qui permet d’ajouter une carte produit à la page
+    function MaCarte(product){
+            document.getElementById('carteParrant').innerHTML+=`<div class="flex card textCenter" id="carte2">
+    <div class="w60">
+        <img src="${product.image}" alt="t-shirt" class="image">
+    </div>
+    <div class="alignCenter w30">
+        <h2 class="top20">${product.title}</h2>
+        <p class="top50">${product.description}</p>
+        <div class="flex top150">
+           <p>${product.price}</p>
+        <a href="" class="btm w60 top5">Ajouter au panier</a> 
+        </div>
+    </div>
+</div>`
+
+    }
